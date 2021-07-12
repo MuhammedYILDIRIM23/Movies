371e73d581e778280836ccfac299db87e6ab42f2
Muhammed YILDIRIM <m.yldrmmm23@gmail.com>
2021-07-12 13:12:31 +0300

    Initial Commit

---
+++ Movies/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Muhammed YILDIRIM  on 12.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
